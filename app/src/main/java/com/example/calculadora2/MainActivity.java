ef83bd81adb387c9f5a5ba6b08a61d128cb08763
Escobaar22 <pescobar@nigul.cide.es>
2023-11-21 17:22:50 +0100

    cambios

--- app/src/main/java/com/example/calculadora2/MainActivity.java
+++ app/src/main/java/com/example/calculadora2/MainActivity.java
@@ -15,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
     String primerNum = "";
     String segundoNum = "";
 
+    Button btnDiv;
+    Button btnMulti;
+    Button btnMas;
+    Button btnMenos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        operacion = findViewById(R.id.textView1);
+
+        // Inicializar botones después de setContentView
+        btnDiv = findViewById(R.id.btnDiv);
+        btnMulti = findViewById(R.id.btnMulti);
+        btnMas = findViewById(R.id.btnMas);
+        btnMenos = findViewById(R.id.btnRes);
+
+        deshabilitarOperaciones();
+    }
+
     public void clickNum(View view){
         Button button = (Button) view;
 
@@ -23,16 +44,50 @@ public class MainActivity extends AppCompatActivity {
 
         // Concatena el texto del botón al valor existente de primerNum
         primerNum += buttonText;
+        segundoNum += buttonText;
 
         // Actualiza el contenido del TextView operacion
-        operacion.setText(primerNum);
+        actualizarTextView();
+
+        if (primerNum.isEmpty()) {
+            deshabilitarOperaciones();
+        } else {
+            habilitarOperaciones();
+        }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void clickOperador(View view){
+        Button button = (Button) view;
 
-        operacion = findViewById(R.id.textView1);
+        String buttonText = button.getText().toString();
+
+        // Si la longitud de operador es mayor que 0, se elimina el último carácter
+        if (operador.length() > 0) {
+            operador = operador.substring(0, operador.length() - 1);
+        }
+
+        operador += buttonText;
+
+        actualizarTextView();
+    }
+
+    private void actualizarTextView(){
+        String operacionMostrar = primerNum + " " + operador + " " + segundoNum;
+
+        operacion.setText(operacionMostrar);
+    }
+
+    private void deshabilitarOperaciones(){
+        btnMas.setEnabled(false);
+        btnMenos.setEnabled(false);
+        btnMulti.setEnabled(false);
+        btnDiv.setEnabled(false);
+    }
+
+    private void habilitarOperaciones(){
+        btnMas.setEnabled(true);
+        btnMenos.setEnabled(true);
+        btnMulti.setEnabled(true);
+        btnDiv.setEnabled(true);
     }
 }
